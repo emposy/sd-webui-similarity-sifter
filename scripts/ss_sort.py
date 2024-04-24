234c857d510dfcc531198a1eae59379fb7c70e0c
masayakitani <kitani.alt@emposy.com>
2024-04-25 03:54:06 +0900

    update save_image

--- scripts/ss_sort.py
+++ scripts/ss_sort.py
@@ -7,7 +7,7 @@ import hashlib
 import re
 
 from modules import scripts, shared, images
-from modules.processing import Processed
+from modules.processing import Processed, StableDiffusionProcessingTxt2Img
 from modules.ui_components import FormColumn
 
 from scripts.ss_config import ss_output_dir
@@ -40,20 +40,20 @@ class SimilaritySifter(scripts.Script):
                     )
                 return [is_enabled, uploaded_image, remove_low_similarity, similarity_threshold]
 
-    def save_images(self, processed: Processed):
+    def save_image(self, p: StableDiffusionProcessingTxt2Img, image, job_timestamp):
         if not os.path.exists(shared.opts.data.get("ss_output_dir", ss_output_dir)):
             os.makedirs(shared.opts.data.get("ss_output_dir", ss_output_dir), exist_ok=True)
-        output_folder = os.path.join(shared.opts.data.get("ss_output_dir", ss_output_dir), f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{processed.seed}")
+        job_datetime = datetime.strftime(datetime.strptime(job_timestamp, "%Y%m%d%H%M%S"), "%Y-%m-%d_%H-%M-%S")
+        output_folder = os.path.join(shared.opts.data.get("ss_output_dir", ss_output_dir), f"{job_datetime}_{p.seed}")
         os.makedirs(output_folder, exist_ok=True)
-        for image in processed.images[1:]:
-            similarity_per = int(image.info.get("SS_similarity", 0) * 1000)
-            seed_match = re.search(r"Seed: (\d+)", image.info.get("parameters", ""))
-            if seed_match:
-                seed = int(seed_match.group(1))
-            else:
-                seed = 0
-            images.save_image(image, path=output_folder, basename="", seed=seed, pnginfo_section_name="parameters", info=image.info.get("parameters", ""), extension=".png", save_to_dirs=False, forced_filename=f"{similarity_per:04d}_{seed}")
-
+        similarity_per = int(image.info.get("SS_similarity", 0) * 1000)
+        seed_match = re.search(r"Seed: (\d+)", image.info.get("parameters", ""))
+        if seed_match:
+            seed = int(seed_match.group(1))
+        else:
+            seed = 0
+        images.save_image(image, path=output_folder, basename="", seed=seed, pnginfo_section_name="parameters", info=image.info.get("parameters", ""), extension="png", save_to_dirs=False, forced_filename=f"{similarity_per:04d}_{seed}")
+    
     def calculate_similarity(self, uploaded_image, generated_image):
         uploaded_encodings = face_recognition.face_encodings(uploaded_image)
         if len(uploaded_encodings) == 0:
@@ -65,14 +65,18 @@ class SimilaritySifter(scripts.Script):
         similarity = face_recognition.face_distance([uploaded_encodings[0]], generated_encodings[0])[0]
         return 1 - similarity
     
-    def postprocess_image(self, p: Processed, pp: scripts.PostprocessImageArgs, is_enabled, uploaded_image, remove_low_similarity, similarity_threshold):
+    def postprocess_image(self, p: StableDiffusionProcessingTxt2Img, pp: scripts.PostprocessImageArgs, is_enabled, uploaded_image, remove_low_similarity, similarity_threshold):
         if not is_enabled or uploaded_image is None:
             return
         similarity = self.calculate_similarity(uploaded_image, pp.image)
+        pp.image.info["parameters"] = Processed(p, []).infotext(p, p.iteration) # 画像情報を先に保存しておく（あとで上書きされる）
         pp.image.info["SS_similarity"] = similarity
         p.extra_generation_params['SS similarity'] = similarity
-        
-    def postprocess(self, p: Processed, processed: Processed, is_enabled, uploaded_image, remove_low_similarity, similarity_threshold):
+
+        if p.n_iter > 1 and (not remove_low_similarity or similarity >= similarity_threshold):
+            self.save_image(p, pp.image, shared.state.job_timestamp)
+    
+    def postprocess(self, p, processed: Processed, is_enabled, uploaded_image, remove_low_similarity, similarity_threshold):
         if not is_enabled or uploaded_image is None:
             return
         max_len = len(processed.infotexts)
@@ -88,5 +92,3 @@ class SimilaritySifter(scripts.Script):
         sorted_indices = np.argsort(filtered_similarities)[::-1]
         processed.images[1:] = [filtered_images[i] for i in sorted_indices] + processed.images[max_len:]
         processed.infotexts[1:] = [filtered_infotexts[i] for i in sorted_indices]
-        if len(similarities) > 1 and len(processed.images[1:]) > 0:
-            self.save_images(processed)
